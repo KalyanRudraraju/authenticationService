038b2ebb950b1fe370ac96be6fba42356955f877
abc <abc@example.com>
2025-07-26 08:29:41 +0530

    first commit

---
+++ AuthenticationService/src/main/java/com/authentication/microservice/repository/RoleRepository.java
@@ -0,0 +1,15 @@
+package com.authentication.microservice.repository;
+
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.authentication.microservice.models.ERole;
+import com.authentication.microservice.models.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+	Optional<Role> findByName(ERole name);
+}
